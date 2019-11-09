7953ae96b7ed93f722da816d44b817a0f6abf335
shiji703 <shijieugin3@gmail.com>
2019-11-08 20:57:59 -0500

    [shiji] new bill

---
+++ C0764796_MidTerm_MAD3115F2019/AddNewBillViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddNewBillViewController.swift
+//  Project
+//
+//  Created by vamsi on 06/11/19.
+//  Copyright © 2019 vamsi. All rights reserved.
+//
+
+import UIKit
+
+protocol AddNewBillViewControllerDelegate:NSObject {
+    func didSelectSaveBtn(_ bill:Bill)
+}
+
+
+
+class AddNewBillViewController: UIViewController {
+    
+    @IBOutlet weak var enterBillIDTextField: UITextField!
+    @IBOutlet weak var enterAmountTextField: UITextField!
+    @IBOutlet weak var enterBillDateTextField: UITextField!
+    @IBOutlet weak var enterBillTypeTextField: UITextField!
+    var pickerView = UIPickerView()
+    var datePicker = UIDatePicker()
+    let toolBar = UIToolbar()
+    let billTypesArray = ["Hydro","Internet","Mobile"]
+  weak var delegate:AddNewBillViewControllerDelegate?
+    var billCopyObj:Bill?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupToolBar()
+        setupPicker()
+        setupTextFields()
+    }
+    
+    
+}
