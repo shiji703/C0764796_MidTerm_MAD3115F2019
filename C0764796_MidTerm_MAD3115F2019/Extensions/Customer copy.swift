4d78adad812cf3bcbee1bc2e5a67ddd574305ef9
shiji703 <shijieugin3@gmail.com>
2019-11-08 19:27:56 -0500

    class customer

---
+++ C0764796_MidTerm_MAD3115F2019/Extensions/Customer copy.swift
@@ -0,0 +1,23 @@
+//
+//  Customer.swift
+//  sample
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Customer
+{
+    
+    
+    var customerId : String
+    var firstName : String
+    var lastName : String!
+    var fullName : String
+    var email : String
+    //var bill : Bill
+    var billDictionary = Dictionary<String,Bill>()
+    
+    
+}
