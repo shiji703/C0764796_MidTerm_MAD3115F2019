595cfdc59e6f6cba7571d9e676b45a844f7e6072
shiji703 <shijieugin3@gmail.com>
2019-11-08 19:31:26 -0500

    [shiji] customer init

--- C0764796_MidTerm_MAD3115F2019/Extensions/Customer copy.swift
+++ C0764796_MidTerm_MAD3115F2019/Extensions/Customer copy.swift
@@ -19,5 +19,18 @@ class Customer
     //var bill : Bill
     var billDictionary = Dictionary<String,Bill>()
     
+    init(customerId : String, firstName : String, lastName : String, email : String ,billDictionary : Dictionary<String,Bill>)
+    {
+        self.customerId = customerId
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+       self.billDictionary = billDictionary
+        self.fullName = firstName + lastName
+        //self.bill = bill
+        
+        //self.billDictionary.updateValue(bill, forKey: bill.billId)
+        
+    }
     
 }
