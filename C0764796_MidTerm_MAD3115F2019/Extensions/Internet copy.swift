fcf5db7f99dfb82d6bef27c65c1c7419927afe62
shiji703 <shijieugin3@gmail.com>
2019-11-08 19:20:16 -0500

    internet class

---
+++ C0764796_MidTerm_MAD3115F2019/Extensions/Internet copy.swift
@@ -0,0 +1,29 @@
+//
+//  Internet.swift
+//  sample
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Internet : Bill
+{
+   
+    
+    var providerName : String
+    var gbUsed : Float
+    var billAmount : Float
+    
+    
+        
+    }
+    
+    
+//    override func totalBill()-> Float {
+//        print("call")
+//        totalBillAmount = totalBillAmount + billAmount
+//        return totalBillAmount
+//    }
+
+}
